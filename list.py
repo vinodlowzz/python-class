71f2fa637bfac8d12790793ca57d34f57f6137f9
vinodlowzz <vinodvld10@gmail.com>
2019-09-18 15:51:10 +0530

    list,set,tuple...

---
+++ list.py
@@ -0,0 +1,8 @@
+lista=["mutable","apple","orange"]
+print(lista)
+tuplea=("a","b","inmutable")
+print(tuplea[1])
+seta={"duplication not allowed","apple"}
+print(seta)
+dicta={"brand":"hublot", "price":"100000" , "made":"india"}
+print(dicta)
